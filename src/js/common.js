91f7f3687d2aa646d38a74b11e5c03193dee14c4
Anton Medvedev <anton@elfet.ru>
2015-04-27 19:59:42 +0700

    Fix parallax.

--- src/js/common.js
+++ src/js/common.js
@@ -5,9 +5,4 @@ $(function () {
     $('pre code[class*="language-"]').each(function (i, block) {
         hljs.highlightBlock(block);
     });
-
-    var header = $('header');
-    var backgroundImage = header.css('background-image').replace('url(', '').replace(')', '');
-    header.parallax({imageSrc: backgroundImage});
-    header.css({background: 'transparent'});
 });
