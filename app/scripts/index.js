d24fc73c0dab774742f8b381cafacf00e643697f
Anton Medvedev <anton@medv.io>
2017-07-16 15:13:17 +0700

    New logo

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,7 +2,33 @@ import "./register-service-worker"
 import highlight from "./highlight"
 
 function ready() {
-  highlight();
+  highlight()
+
+  if (window.navigator.userAgent.indexOf('Windows') !== -1) {
+    document.documentElement.className = '-windows'
+  } else {
+    document.documentElement.className = '-macos'
+  }
+
+
+  const logo = document.querySelector('.logo')
+  const people = ['😜', '🙃', '😉', '😂', '😊', '🙂', '😲', '😝', '😋', '😌', '😍', '😳']
+
+  let i = 0
+  const next = () => people[i++ % people.length]
+
+  if (logo) {
+    const tspan = logo.querySelector('tspan')
+    if (tspan) {
+      let interval = null
+      const roll = () => tspan.textContent = next()
+      const start = () => interval = setInterval(roll, 100)
+      const stop = () => clearInterval(interval)
+
+      logo.addEventListener('mouseenter', start)
+      logo.addEventListener('mouseleave', stop)
+    }
+  }
 }
 
 if (document.readyState !== 'loading') {
